bcb1b5d5f22176e587f11c0c482822fabe5fdb59
unknown <rebeluna263@gmail.com>
2023-11-05 22:40:17 -0300

    se agregaron los estilos para el navbar mobile y desktop

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,6 +4,20 @@ bars.onclick = function() {
     navBar.classList.toggle("active")
 }
 
+/* window.onscroll = function(){
+    const navbar = document.getElementById('navbar');
+    const btnNavBar = document.getElementById('btn-navbar');
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+    btnNavBar.addEventListener('click', function(){
+        if(document.body.scrollTop === 0 && document.documentElement.scrollTop === 0){
+            navbar.classList.add('scrolled');
+        }
+    });
+} */
 //AOS
 AOS.init({
     duration:1500,
